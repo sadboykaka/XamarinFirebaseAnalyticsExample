8c808fcfcc3d08066d85d29d5d4d42f9dc48ab97
Ilan <ilanolkies@outlook.com>
2018-05-08 22:28:37 -0300

    Create events page.
    EventsPage with 3 buttons to be used to log events.

---
+++ FAnalyticsExample/EventsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace FAnalyticsExample
+{
+	public partial class EventsPage : ContentPage
+    {
+        public EventsPage()
+        {
+			InitializeComponent();
+
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+		}
+
+        public void Event1_Clicked(object sender, EventArgs e)
+        {
+			
+        }
+
+        public void Event2_Clicked(object sender, EventArgs e)
+        {
+			
+        }
+
+        public void Event3_Clicked(object sender, EventArgs e)
+        {
+			
+        }
+    }
+}
--- FAnalyticsExample/MainPage.xaml.cs
+++ FAnalyticsExample/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace FAnalyticsExample
 {
-    public partial class MainPage : ContentPage
+	public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
